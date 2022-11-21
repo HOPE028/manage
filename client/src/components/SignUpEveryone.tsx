76dacf8b306ebc36d31e98f37df5858aa621fc7a
Pasha Khoshkebari <pkhoshkeba@smail.pembinatrails.ca>
2022-11-20 19:42:31 -0600

    Problem and trying to sovle

--- client/src/components/SignUpEveryone.tsx
+++ client/src/components/SignUpEveryone.tsx
@@ -1,17 +1,53 @@
 import React, { useState, useEffect } from 'react'
 import { db } from '../firebase-config'
 import { useAuth } from '../contexts/AuthContext'
+import { collection, addDoc, setDoc, doc, getDocs } from 'firebase/firestore'
 
 interface InterfaceSignUpEveryone {
   role: 'Manager' | 'Member'
 }
 
 export default function SignUpEveryone(props: InterfaceSignUpEveryone) {
-  const { currentUserInformation } = useAuth()
+  const { currentUser, currentUserInformation } = useAuth()
+
+  const [originalFields, setOriginalFields] = useState()
+  const [customFields, setCustomFields] = useState()
+
+  useEffect(() => {
+    console.log(currentUserInformation)
+
+    if (currentUserInformation) {
+      getData()
+    }
+  }, [])
+
+  const getData = async () => {
+    // const refOriginalFields = collection(
+    //   db,
+    //   'Class',
+    //   currentUserInformation.teamCode,
+    //   'Rules',
+    //   'Information_Required',
+    //   'Original'
+    // )
+
+    // const refCustomFields = collection(
+    //   db,
+    //   'Class',
+    //   currentUserInformation.teamCode,
+    //   'Rules',
+    //   'Information_Required',
+    //   'Custom'
+    // )
+
+    // const data = await getDocs(refOriginalFields)
+    console.log('hello')
+
+    // setOriginalFields(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+  }
   return (
     <div>
       <h1>Sign Up!</h1>
-      <button onClick={() => console.log(currentUserInformation)}>Yes</button>
     </div>
   )
 }
