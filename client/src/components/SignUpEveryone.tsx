46ae2837c8e8eab81f8e01825e1d2cdcd386cca9
Pasha Khoshkebari <pkhoshkeba@smail.pembinatrails.ca>
2022-11-22 21:58:24 -0600

    Still working

--- client/src/components/SignUpEveryone.tsx
+++ client/src/components/SignUpEveryone.tsx
@@ -1,21 +1,47 @@
 import React, { useState, useEffect } from 'react'
 import { db } from '../firebase-config'
 import { useAuth } from '../contexts/AuthContext'
-import { collection, addDoc, setDoc, doc, getDocs } from 'firebase/firestore'
+import {
+  collection,
+  addDoc,
+  setDoc,
+  doc,
+  getDocs,
+  QueryDocumentSnapshot,
+  DocumentData,
+} from 'firebase/firestore'
 
 interface InterfaceSignUpEveryone {
   role: 'Manager' | 'Member'
 }
 
+interface InterfaceOriginalInformation {
+  id: string
+  label: string
+  name: string
+  value: boolean
+  valueType: string
+}
+
+interface InterfaceCustomInformation {
+  id: string
+  name: string
+  valueType: string
+}
+
 export default function SignUpEveryone(props: InterfaceSignUpEveryone) {
   const { currentUser, currentUserInformation } = useAuth()
 
-  let any1: any
-  any1 = {}
-  let any2: any
-  any2 = {}
-  const [originalFields, setOriginalFields] = useState(any1)
-  const [customFields, setCustomFields] = useState(any2)
+  // let any1: InterfaceInformation
+  // any1 = { id: 'random' }
+  // let any2: InterfaceInformation
+  // any2 = { id: 'random' }
+  const [originalFields, setOriginalFields] = useState<
+    QueryDocumentSnapshot<DocumentData>[]
+  >([])
+  const [customFields, setCustomFields] = useState<
+    QueryDocumentSnapshot<DocumentData>[]
+  >([])
 
   useEffect(() => {
     console.log(currentUserInformation)
@@ -44,23 +70,29 @@ export default function SignUpEveryone(props: InterfaceSignUpEveryone) {
     )
 
     const originalData = await getDocs(refOriginalFields)
+
+    // console.log(JSON.stringify(info))
+
     setOriginalFields(
       originalData.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
     )
 
-    const customData = await getDocs(refOriginalFields)
-    setOriginalFields(
+    const customData = await getDocs(refCustomFields)
+    setCustomFields(
       customData.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
     )
   }
   return (
     <div>
       <h1>Sign Up!</h1>
-      {customFields.map((field) => {
-        ;<div key={field.id}>
-          <h2>{field.name}</h2>
-        </div>
-      })}
+      {/* <div>
+        {customFields.map((field) => {
+          ;<div key={field.id}>
+            <h2>{field.name}</h2>
+          </div>
+        })}
+      </div> */}
+      <button onClick={() => console.log(customFields)}>DATA</button>
     </div>
   )
 }
